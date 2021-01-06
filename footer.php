1a1765b88b5aa4988478e7767a4ff9226130f7f5
Bartek <submission93@gmail.com>
2021-01-06 19:42:57 +0100

    footer completed

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <footer class="footer">
     <div class="footer__container">
-        <nav class="footer__nav">
-            <ul class="footer__ulist">
+
+        <!-- <ul class="footer__ulist">
                 <li class="footer__list-item"><a class="footer__link" href="<?php echo site_url('home') ?>">Home</a>
                 </li>
                 <li class="footer__list-item"><a class="footer__link"
@@ -13,15 +13,15 @@
                 <li class="footer__list-item"><a class="footer__link" href="">Jak dołączyć</a></li>
                 <li class="footer__list-item"><a class="footer__link"
                         href="<?php echo get_post_type_archive_link('blog') ?>">Blog</a></li>
-            </ul>
-        </nav>
+            </ul> -->
+
 
         <p class="footer__copyright">
             Copyright &copy; by Bartłomiej Ostrowski 2020. Wszelkie prawa zastrzeżone.
         </p>
 
 
-        <div class="social-features">
+        <section class="social-features">
             <div class="icons">
                 <a href="https://www.youtube.com/user/KontratakGrupa"><img
                         src="<?php echo get_theme_file_uri('images/youtube.svg') ?>" alt=""></a>
@@ -34,7 +34,7 @@
 
             </div>
 
-        </div>
+        </section>
 
     </div>
 
